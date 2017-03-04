c0781db4383b8abef676c21617681316e9a77823
JonnyRivers <jonny.w.rivers@gmail.com>
2017-03-04 12:38:48 -0800

    Adding Some CSS Styles

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 var app = angular.module('flapperNews', []);
 
+app.factory('posts', [function(){
+	var o = {
+		posts: [
+			{title: 'post 1', upvotes: 5},
+		  	{title: 'post 2', upvotes: 2},
+		  	{title: 'post 3', upvotes: 15},
+		  	{title: 'post 4', upvotes: 9},
+		  	{title: 'post 5', upvotes: 4}
+		]
+	};
+
+	return o;
+}])
+
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
   $scope.test = 'Hello world!';
   $scope.title = '';
   $scope.link = '';
 
-  $scope.posts = [
-  	{title: 'post 1', upvotes: 5},
-  	{title: 'post 2', upvotes: 2},
-  	{title: 'post 3', upvotes: 15},
-  	{title: 'post 4', upvotes: 9},
-  	{title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = posts.posts;
 
   $scope.addPost = function() {
   	if ($scope.title === '') {
