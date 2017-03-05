605f49fccb71567adc3c3d40852b56738e0cdfc1
JonnyRivers <jonny.w.rivers@gmail.com>
2017-03-05 06:34:17 -0800

    Opening REST Routes

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -5,6 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+require('./models/Comments');
+require('./models/Posts');
+
+mongoose.connect('mongodb://localhost/news');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -42,11 +48,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-var mongoose = require('mongoose');
-require('./models/Comments');
-require('./models/Posts');
-
-mongoose.connect('mongodb://localhost/news');
 
 module.exports = app;
